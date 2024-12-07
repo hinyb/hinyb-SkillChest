51f602d6b7b28f33c73e3bdb340400c881118b22
hinyb <40139991+hinyb@users.noreply.github.com>
2024-12-07 13:55:30 +0800

    fix  fragile's bug

--- SkillModifiers/fragile.lua
+++ SkillModifiers/fragile.lua
@@ -5,6 +5,12 @@ SkillModifier.register_modifier("fragile", 250, nil, function(skill, data)
     SkillModifier.add_on_activate_callback(data, function(skill_)
         if skill_.parent.is_local then
             if Utils.get_random() < 0.05 then
+                if skill_.ctm_arr_modifiers then
+                    local ctm_arr_modifiers = Array.wrap(skill_.ctm_arr_modifiers)
+                    for i = 0, ctm_arr_modifiers:size() - 1 do
+                        SkillModifier.remove_modifier(skill_, i + 1, ctm_arr_modifiers:get(i):get(0))
+                    end
+                end
                 gm.actor_skill_set(skill_.parent, skill_.slot_index, 0)
             end
         end
